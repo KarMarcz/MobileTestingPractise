56c80951cd7abaaf0f382628c3cdfd2950c5592b
Karol Marczyk <KarMarcz@gmail.com>
2019-02-16 18:01:10 +0100

    After setting env

---
+++ src/main/java/mobileApp/MobileApp.java
@@ -0,0 +1,7 @@
+package mobileApp;
+
+public class MobileApp {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
---
+++ src/test/java/mobileAppTest/MobileAppTest.java
@@ -0,0 +1,4 @@
+package mobileAppTest;
+
+public class MobileAppTest {
+}
